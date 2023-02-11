f15c9a3747509cfb1c861b5e0b9811ba4274ecc5
Gabriel Orlando <gabriel.orlando00@gmail.com>
2023-02-10 21:48:03 -0300

    Feat: Add funcionalidade de Logs; Add tela de Login/Cadastro de usuários; [Em andamento] Scripts de login (JS e PHP) | close #16

--- blog/src/v1.0/js/login.js
+++ blog/src/v1.0/js/login.js
@@ -1,12 +1,120 @@
+$(document).ready( () => {
+    events();
+    carregaCredenciais();
+});
+
+function events(){
+    $('#btn_login').on('click', () => {
+        buscarUsuario(checkLogin()) == 'Usuário ou senha incorreta.' ? alert('Usuário ou senha incorreta.') : true;        
+    });
+
+    $('#btn_cadastro').on('click', () => {
+        $('#form_login').hide();
+        $('#form_cadastro').show();
+    });
+
+    $('#btn_novo_usuario').on('click', () => {
+
+    });
+}
+
+function carregaCredenciais(){
+    let email = localStorage.getItem('login');
+    let pass = localStorage.getItem('pass');
+
+    if(email && pass){
+        $('#login_txt_email').val(email);
+        $('#login_txt_password').val(pass)
+    }
+}
+
 function buscarUsuario(login){
+    let usuario;
+
     if(!login){
         console.error('Usuário ou Senha incorretos.');
         alert('Usuário ou Senha incorretos.');
     } else {
-        alert('Ok')
+        // alert('Ok')
+        $.ajax({
+            url: "php/user/login.php",
+            type: "POST",
+            data: {
+                'usuario': $('#login_txt_email').val().trim(),
+                'senha' : $('#login_txt_password').val().trim()
+            },
+            async: false,
+            success: function (data) {
+                console.log(data);
+                usuario = data;
+            },
+            error: function (e) {
+                console.log(e)
+            }
+        });
     }
+
+    return usuario;
 }
 
 function checkLogin(){
-    return $('#txt_email').val().trim() != '' && $('#txt_pass').val().trim() != '' ? true : false;
+    if($('#login_txt_email').val().trim() != '' && $('#login_txt_password').val().trim() != ''){
+
+        if($('#ckb_remember').prop("checked") == "checked"){
+            localStorage.setItem('login') = $('#login_txt_email').val().trim();
+            localStorage.setItem('pass') = $('#login_txt_password').val().trim();
+        }
+
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkCadastro(){
+    if($('#cad_txt_email').val().trim() != ''){
+        if($('#cad_txt_nome').val().trim() != ''){
+            if($('#cad_txt_sobrenome').val().trim() != ''){
+                if($('#cad_password').val().trim() != ''){
+                    if($('#cad_password_confirm').val().trim() != ''){
+                        if($('#cad_password').val().trim() == $('#cad_password_confirm').val().trim()){
+                            if($('#ckb_termos').prop("checked") == "checked"){
+                                $.ajax({
+                                    url: "PHP/user/cadastro.php",
+                                    type: "POST",
+                                    data: {
+                                        'email': $('#cad_txt_email').val().trim(),
+                                        'nome' :  $('#cad_txt_nome').val().trim(),
+                                        'sobrenome' : $('#cad_txt_sobrenome').val().trim(),
+                                        'senha' : $('#cad_password').val().trim()
+                                    },
+                                    async: false,
+                                    success: function (data) {
+                                        console.log("Login!")
+                                    },
+                                    error: function (e) {
+                                        console.log(e)
+                                    }
+                                });
+                            } else {
+                                $('#ckb_termos').focus();
+                            }
+                        } else {
+                            alert("Erro senhas incompativeis")
+                        }
+                    } else {
+                        $('#cad_password_confirm').focus();
+                    }
+                } else {
+                    $('#cad_password').focus();
+                }
+            } else {
+                $('#cad_txt_sobrenome').focus();
+            }
+        } else {
+            $('#cad_txt_nome').focus();
+        }
+    } else {
+        $('#cad_txt_email').focus();
+    }
 }
